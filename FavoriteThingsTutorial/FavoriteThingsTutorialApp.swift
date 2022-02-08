8b0fa454fcc0675690cd760bf3970de3efeadb7a
hamitseyrek <hamitseyrek@gmail.com>
2022-02-08 21:55:09 +0300

    Initial Commit

---
+++ FavoriteThingsTutorial/FavoriteThingsTutorialApp.swift
@@ -0,0 +1,17 @@
+//
+//  FavoriteThingsTutorialApp.swift
+//  FavoriteThingsTutorial
+//
+//  Created by Hamit Seyrek on 8.02.2022.
+//
+
+import SwiftUI
+
+@main
+struct FavoriteThingsTutorialApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
